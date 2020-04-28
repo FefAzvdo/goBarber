60d618b74b0e3f7e5c49e9658ad5752f7c92aed7
FefAzvdo <wfernandoaugusto01@gmail.com>
2020-04-28 14:11:32 -0300

    Listagem de prestadores de serviço, Migration e model de agendamento, agendamento de serviço

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,7 +4,9 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-import FileControler from './app/controllers/FileController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -16,8 +18,12 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
+routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
+
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), FileControler.store);
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
